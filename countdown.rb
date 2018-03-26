685e2403b5d6b59c200d61561ab2b6c878a3f4d5
Aleksandr Roslyakov <torpid@gmail.com>
2018-03-26 19:58:34 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,9 @@
 #write your code here
 
-def countdown
+def countdown(number_of_seconds)
+  counter = number_of_seconds
+  while counter > 0
+  puts "#{counter} SECOND(S)!"
+  counter -= 1
+  end
 end
